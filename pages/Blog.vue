d36fe8d2d03a6dd6e3c7da94d686473a41a5be87
Immortalbug <stephenvicson@gmail.com>
2022-03-19 22:40:22 +0200

    Blog section added

--- pages/Blog.vue
+++ pages/Blog.vue
@@ -1,8 +1,51 @@
 <template>
-<div>
-  <h1 class="text-center text-xl  text-blue-600">Blo
-  </h1>
-
+<div class="px-16 ">
+  <section class="my-12 ">
+    <h1 class=" font-bold text-4xl mb-4 text-blue-900 ">Articles</h1>
+    <div class="divide-y divide-dotted text-gray-600 divide-blue-900 divide-y-16 space-y-4">
+      <article class="">
+        <div class="sm:flex sm:items-center sm:justify-between mb-4">
+          <h3 class="font-semibold text-2xl ">
+            <a href="#" class="hover-underline hover:text-blue-900 ">I'm always happy to learn new skill </a>
+          </h3>
+          <span class="">
+            5 mins read
+          </span>
+        </div>
+        <div>
+          <p class="tracking-wider leading-loose">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </article>
+      <article class="">
+        <div class="sm:flex sm:items-center sm:justify-between mb-4">
+          <h3 class="font-semibold text-2xl ">
+            <a href="#" class="hover-underline hover:text-blue-900 ">I'm always happy to learn new skill to find the best solutions</a>
+          </h3>
+          <span class="">
+            5 mins read
+          </span>
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </article>
+      <article class="">
+        <div class="sm:flex sm:items-center sm:justify-between mb-4">
+          <h3 class="font-semibold text-2xl ">
+            <a href="#" class="hover-underline hover:text-blue-900 ">I'm always happy to learn new skill to find the best solutions</a>
+          </h3>
+          <span class="">
+            5 mins read
+          </span>
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </article>
+    </div>
+  </section>
 </div>
 </template>
 
