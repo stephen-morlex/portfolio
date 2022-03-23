6264c853cda359f8dc5dcf5e753252922e081c52
Immortalbug <stephenvicson@gmail.com>
2022-03-23 20:08:41 +0200

    refactored Blog and content blog

--- pages/Resume.vue
+++ pages/Resume.vue
@@ -2,7 +2,9 @@
   <div class="px-6 sm:px-56 ">
     <section class="my-12 ">
       <h1 class=" font-bold text-4xl mb-4 text-blue-900 ">Resume</h1>
-      <iframe loading="lazy" src="~/assets/cv.pdf" width="100%" height="1000" ></iframe>
+      <iframe loading="lazy" src="~/assets/dp.png" width="100%" height="1000" >
+        
+      </iframe>
     </section>
   </div>
 </template>
